edad910fb64604595fc5883af83098a77067135d
javierbrea <javier.brea@gmail.com>
2025-10-17 14:14:12 +0200

    chore: Add missing license headers

--- components/confluence-sync/src/confluence/errors/ErrorHelpers.ts
+++ components/confluence-sync/src/confluence/errors/ErrorHelpers.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Telefónica Innovación Digital
+// SPDX-License-Identifier: Apache-2.0
+
 /**
  * Returns the error message from the cause if it is an instance of Error, otherwise returns the cause itself.
  * @param cause Cause of an error. It might be another error, or a string usually
